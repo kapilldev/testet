345b2e4ff6c80b5aac7865ff3c82a11c878e9fff
Kapil Dev <140963838+kapilldev@users.noreply.github.com>
2025-02-26 12:48:55 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,23 @@
 
+.button {
+    width: 100%;
+    background: #1a73e8;
+    color: white;
+    border: none;
+    padding: 10px;
+    border-radius: 5px;
+    font-size: 14px;
+    cursor: pointer;
+    margin-top: 10px;
+  }
+  
+  .button:hover {
+    background: #1666c1;
+  }
+  
+  .button-small {
+    width: auto;
+    padding: 10px 10px;
+    margin : 20px
+  }
+  
